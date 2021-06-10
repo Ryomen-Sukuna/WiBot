f43b6cb0a9fe1f56b851914d330ebd0b73bd8d21
Ihsan <78595134+Scroxy-X@users.noreply.github.com>
2021-06-10 07:48:09 +0700

    update whois.py

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -22,7 +22,7 @@ from userbot.events import register
 async def who(event):
 
     await event.edit(
-        "`Sit tight while I steal some data from *Global Network Zone*...`"
+        "**FBI Open The Door!!** `OPEN NOW.....`"
     )
 
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
@@ -143,18 +143,18 @@ async def fetch_info(replied_user, event):
     user_bio = "This User has no About" if not user_bio else user_bio
 
     caption = "<b>USER INFO:</b>\n\n"
-    caption += f"First Name: {first_name}\n"
-    caption += f"Last Name: {last_name}\n"
-    caption += f"Username: {username}\n"
-    caption += f"Data Centre ID: {dc_id}\n"
-    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"Is Bot: {is_bot}\n"
-    caption += f"Is Restricted: {restricted}\n"
-    caption += f"Is Verified by Telegram: {verified}\n"
-    caption += f"ID: <code>{user_id}</code>\n\n"
-    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Common Chats with this user: {common_chat}\n"
-    caption += f"Permanent Link To Profile: "
+    caption += f"👤**First Name**: {first_name}\n"
+    caption += f"📍**Last Name**: {last_name}\n"
+    caption += f"🧲**Username**: {username}\n"
+    caption += f"🌐**Data Centre ID**: {dc_id}\n"
+    caption += f"💈**Number of Profile Pics**: {replied_user_profile_photos_count}\n"
+    caption += f"🤖**Is Bot**: {is_bot}\n"
+    caption += f"⛓**Is Restricted**: {restricted}\n"
+    caption += f"✅**Is Verified by Telegram**: {verified}\n"
+    caption += f"🩸**ID**: <code>{user_id}</code>\n\n"
+    caption += f"🧬**Bio**: \n<code>{user_bio}</code>\n\n"
+    caption += f"🦠**Common Chats with this user**: {common_chat}\n"
+    caption += f"🔮**Permanent Link To Profile**: "
     caption += f'<a href="tg://user?id={user_id}">{first_name}</a>'
 
     return photo, caption
