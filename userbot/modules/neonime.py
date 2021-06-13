1af319f22717076be8fd9fd543447d80bee5bf29
EruBot <64939964+EruBot@users.noreply.github.com>
2021-06-13 21:50:33 +0700

    Update neonime.py

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -42,7 +42,29 @@ async def _neonime(event):
     ht_ = requests.get(url).text
     _bs = bs(ht_, "html.parser")
     bd_ = _bs.findAll('h2', class_='episodeye')
-    out = "<b>➲ Movie Terbaru:</b>\n═════════════════\n"
+    out = "<b>➲ Batch Terbaru:</b>\n═════════════════\n"
+    for kntl_ in bd_:
+        _lucu = kntl_.find('a')
+        if not _lucu:
+            _lucu = 'none'
+        else:  # FKTnK3aKtFvMSUiWLZrTuAp4g93VSjbXcR5zGmqWAijuAuYgR2ACP8WNot2ZyTRVECks1uV5WWW7muWz5SZkY2P8YbWW6AYLUFTsmFU1oW9Y2GP4
+            tt_ = _lucu.get_text()
+            _tt = re.sub(r'\s+Subtitle\s+Indonesia\s+Season.\d+', '', tt_)
+            link = _lucu['href']
+            out += f"➣ <a href='{link}'>{_tt}</a>\n"
+            if len(out) > 1000:
+                break
+            await event.edit(out, parse_mode="html")
+
+
+@register(outgoing=True, pattern=r"^\.act ?(.*)")
+async def _neonime(event):
+    await event.edit('tunggu bentar...')
+    url = 'https://kusonime.com/'
+    ht_ = requests.get(url).text
+    _bs = bs(ht_, "html.parser")
+    bd_ = _bs.findAll('div', class_='venz')
+    out = "<b>➲ Batch Action:</b>\n═════════════════\n"
     for kntl_ in bd_:
         _lucu = kntl_.find('a')
         if not _lucu:
