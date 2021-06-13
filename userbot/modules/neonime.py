7eeffc1d488f120f6b0fde2d3d1b2b5cf706bce8
EruBot <64939964+EruBot@users.noreply.github.com>
2021-06-13 21:14:24 +0700

    Update neonime.py

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -35,13 +35,13 @@ async def _neonime(event):
             await event.edit(out, parse_mode="html")
 
 
-@register(outgoing=True, pattern=r"^\.nm ?(.*)")
+@register(outgoing=True, pattern=r"^\.nb ?(.*)")
 async def _neonime(event):
     await event.edit('tunggu bentar...')
-    url = 'https://neonime.site/movies/'
+    url = 'https://neonime.site/noticias/'
     ht_ = requests.get(url).text
     _bs = bs(ht_, "html.parser")
-    bd_ = _bs.findAll('div', class_='item_1 items')
+    bd_ = _bs.findAll('div', class_='noticias')
     out = "<b>➲ Movie Terbaru:</b>\n═════════════════\n"
     for kntl_ in bd_:
         _lucu = kntl_.find('a')
