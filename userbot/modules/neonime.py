bafe1e7b85f1aee9f3f578970434b7f5d172ba8f
EruBot <64939964+EruBot@users.noreply.github.com>
2021-06-19 06:24:43 +0700

    Update neonime.py

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -111,8 +111,7 @@ async def _neonime(event):
     out = "<b>➲ Owibu > Info Terbaru:</b>\n═════════════════\n"
     for kntl_ in bd_:
         _lucu = kntl_.find('a')
-        if not _lucu:
-            _lucu = 'none'
+
         else:  # FKTnK3aKtFvMSUiWLZrTuAp4g93VSjbXcR5zGmqWAijuAuYgR2ACP8WNot2ZyTRVECks1uV5WWW7muWz5SZkY2P8YbWW6AYLUFTsmFU1oW9Y2GP4
             tt_ = _lucu.get_text()
             _tt = re.sub(r'\s+TT\s+TTT\s+TTTT.\d+', '', tt_)
