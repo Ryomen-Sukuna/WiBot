f4577aa43a1628c8c52d508a567e33db08fa98a1
EruBot <64939964+EruBot@users.noreply.github.com>
2021-06-19 06:31:07 +0700

    Update neonime.py

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -118,7 +118,8 @@ async def _neonime(event):
             _tt = re.sub(r'\s+TT\s+TTT\s+TTTT.\d+', '', tt_)
             link = _lucu['href']
             out += f"➣ <a href='{link}'>{tt_}</a>\n"
-
+            if len(out) > 1000:
+                break
             await event.edit(out, parse_mode="html")
 
 
