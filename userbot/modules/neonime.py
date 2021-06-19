6279cbf19e3289ead64ea4a708223e17c5532b18
EruBot <64939964+EruBot@users.noreply.github.com>
2021-06-19 07:11:58 +0700

    Update neonime.py

--- userbot/modules/neonime.py
+++ userbot/modules/neonime.py
@@ -110,14 +110,15 @@ async def _neonime(event):
     bd_ = _bs.findAll('div', class_='section-content clearfix')
     out = "<b>➲ Jurnal Otaku > Info Terbaru:</b>\n═════════════════\n"
     for kntl_ in bd_:
-        _lucu = kntl_.find('a')
+        _lucu = kntl_.find('img')
         if not _lucu:
             _lucu = 'none'
         else:  # FKTnK3aKtFvMSUiWLZrTuAp4g93VSjbXcR5zGmqWAijuAuYgR2ACP8WNot2ZyTRVECks1uV5WWW7muWz5SZkY2P8YbWW6AYLUFTsmFU1oW9Y2GP4
             tt_ = _lucu.get_text()
             _tt = re.sub(r'\s+http://jurnalotaku.com/\s+/\s+-.\d+', '', tt_)
-            link = _lucu['href']
-            out += f"➣ <a href='{link}'>{_tt}</a>\n"
+            link = _lucu['src']
+            judul = _lucu['alt']
+            out += f"➣ <a href='{link}'>{judul}</a>\n"
             if len(out) > 1000:
                 break
             await event.edit(out, parse_mode="html")
